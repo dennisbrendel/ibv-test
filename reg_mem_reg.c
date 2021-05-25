0db5ed29115bfe9f64f977df7e70b3df988523a8
Dennis Brendel <mail@dennis-brendel.de>
2021-05-25 08:40:13 +0200

    Make IB device selectable
    Put the initialization into a separate function, so that we - in the end
    - have all the steps needed for requesting a memory region separated
    from each other.
    
    For ease of use mae the relevant ibv structs global.

--- reg_mem_reg.c
+++ reg_mem_reg.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <infiniband/verbs.h>
 
-int main(int argc, char *argv[]) {
+struct ibv_device **ib_devices = NULL;
+
+int init(int dev_id) {
   int num_devices = 0;
-  struct ibv_device **ib_devices = NULL;
 
   ib_devices = ibv_get_device_list(&num_devices);
   if (ib_devices == NULL) {
@@ -20,6 +22,30 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < num_devices; i++) {
     printf("Found IB device with ID %d: %s\n",
            i,  ibv_get_device_name(ib_devices[i]));
+  }
+
+  if (dev_id > num_devices - 1) {
+    fprintf(stderr, "Failed to use device with ID %d, falling back to %d\n",
+                    dev_id, num_devices - 1);
+    dev_id = num_devices - 1;
+  }
+
+  printf("Using IB device with ID %d\n", dev_id);
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  int res = 0;
+  int dev_id = 0;
+
+  if (argc > 1) {
+    dev_id = atoi(argv[1]);
+  }
+
+  res = init(dev_id);
+  if (res != 0) {
+    fprintf(stderr, "Failed to initialize\n");
   }
 
   ibv_free_device_list(ib_devices);
